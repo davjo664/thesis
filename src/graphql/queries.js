2d9bdb202b76d2e1c9c96ba5e87c9131bd8ab2e7
davjo664 <davjo664@gmail.com>
2019-08-08 15:07:07 +0200

    Added missing fields in GraphQL query.

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -3,10 +3,15 @@ export const USERS_QUERY = gql`
     query UsersQuery($page: Int!, $search_term: String!, $sort: String, $order: String, $role_filter_id: String) {
       users(page: $page, search_term: $search_term, sort: $sort, order: $order, role_filter_id: $role_filter_id) {
         users {
+          id,
+          name,
           sortable_name,
           short_name,
           email,
-          time_zone
+          time_zone,
+          avatar_url,
+          last_login,
+          sis_user_id
         },
         links {
           current,
