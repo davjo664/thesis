5c0505c80af870403fec4ce33ad023a0cda742ba
davjo664 <davjo664@gmail.com>
2019-10-31 09:36:48 +0100

    Removed UsersSearch which is not needed anymore.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import UsersSearch from './UsersSearch';
+import UsersPane from './components/UsersPane';
 import configureStore from './store/configureStore'
 import initialState from './store/initialState'
 import './index.css'
 import '@instructure/canvas-theme'
 
 const store = configureStore(initialState);
-ReactDOM.render(<Provider store={store}><UsersSearch /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><UsersPane /></Provider>, document.getElementById('root'));
 
